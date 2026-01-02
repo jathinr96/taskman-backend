9291d164802456608634bd12c4526672bfbd91db
jathinr96 <jathinr96@gmail.com>
2026-01-03 02:58:41 +0530

    feat: adde the search route and secured it with authentication check

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/authController');
+const { registerUser, loginUser, searchUsers } = require('../controllers/authController');
+const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get('/search', protect, searchUsers);
 
 module.exports = router;
+
+
